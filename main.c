a6296c889021eceae7f50939c508fbb15b0f6dbb
Olive (Mathias C.) <117227548+Olivi3rCar@users.noreply.github.com>
2025-11-23 00:09:42 +0100

    ouais ?

--- main.c
+++ main.c
@@ -24,28 +24,28 @@ int main() {
     p_adjlist test_adj = createAdjList(5);
     // for this example we use the same as in presented in the pdf
     // 1 (effectively 0): sunny
-    addCell(test_adj->listarray[0],0,(float)0.34);
-    addCell(test_adj->listarray[0],1,(float)0.27);
-    addCell(test_adj->listarray[0],3,(float)0.18);
-    addCell(test_adj->listarray[0],4,(float)0.21);
+    addCell(test_adj->listarray[0], 0, (float) 0.34);
+    addCell(test_adj->listarray[0], 1, (float) 0.27);
+    addCell(test_adj->listarray[0], 3, (float) 0.18);
+    addCell(test_adj->listarray[0], 4, (float) 0.21);
     // 2 (1) : cloudy
-    addCell(test_adj->listarray[1],0,(float)0.20);
-    addCell(test_adj->listarray[1],1,(float)0.40);
-    addCell(test_adj->listarray[1],2,(float)0.20);
-    addCell(test_adj->listarray[1],4,(float)0.20);
+    addCell(test_adj->listarray[1], 0, (float) 0.20);
+    addCell(test_adj->listarray[1], 1, (float) 0.40);
+    addCell(test_adj->listarray[1], 2, (float) 0.20);
+    addCell(test_adj->listarray[1], 4, (float) 0.20);
     // 3 (2) : rain
-    addCell(test_adj->listarray[2],1,(float)0.41);
-    addCell(test_adj->listarray[2],2,(float)0.37);
-    addCell(test_adj->listarray[2],3,(float)0.09);
-    addCell(test_adj->listarray[2],4,(float)0.13);
+    addCell(test_adj->listarray[2], 1, (float) 0.41);
+    addCell(test_adj->listarray[2], 2, (float) 0.37);
+    addCell(test_adj->listarray[2], 3, (float) 0.09);
+    addCell(test_adj->listarray[2], 4, (float) 0.13);
     // 4 (3) : storm
-    addCell(test_adj->listarray[3],1,(float)0.68);
-    addCell(test_adj->listarray[3],2,(float)0.20);
-    addCell(test_adj->listarray[3],3,(float)0.12);
+    addCell(test_adj->listarray[3], 1, (float) 0.68);
+    addCell(test_adj->listarray[3], 2, (float) 0.20);
+    addCell(test_adj->listarray[3], 3, (float) 0.12);
     // 5 (4) : sunny spells
-    addCell(test_adj->listarray[4],0,(float)0.12);
-    addCell(test_adj->listarray[4],1,(float)0.30);
-    addCell(test_adj->listarray[4],4,(float)0.58);
+    addCell(test_adj->listarray[4], 0, (float) 0.12);
+    addCell(test_adj->listarray[4], 1, (float) 0.30);
+    addCell(test_adj->listarray[4], 4, (float) 0.58);
 
 
     displayAdjList(*test_adj);
@@ -59,7 +59,8 @@ int main() {
     p_mat m3 = mult_matrix(*thegmat, *m2);
     printf("this is M^3 :\n");
     display_matrix(*m3);
-    free_matrix(m2); free_matrix(m3);
+    free_matrix(m2);
+    free_matrix(m3);
     // get m^7
     p_mat mn = zero_matrix(thegmat->len);
     copy_matrix(*thegmat, mn);
@@ -69,10 +70,12 @@ int main() {
         free_matrix(tocopythentofree);
     }
     printf("this is M^7 :\n");
-    display_matrix(*mn); free_matrix(mn);
+    display_matrix(*mn);
+    free_matrix(mn);
 
     // min value of n for which m^n - m^n-1 <= 0.01
-    float epsilon = 1; int n = 1;
+    float epsilon = 1;
+    int n = 1;
     mn = zero_matrix(thegmat->len);
     copy_matrix(*thegmat, mn);
     while (epsilon > 0.01) {
